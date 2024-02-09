c93ab0eb1577dbe4d506eb496e1a6cc680a30def
gkNoel <noelgk@hotmail.com>
2024-02-09 14:03:53 -0500

    Added FindHyp class

---
+++ Calculator/src/math/FindHyp.java
@@ -0,0 +1,14 @@
+package math;
+
+public class FindHyp extends Operator{
+
+	public FindHyp(float a, float b) {
+		super(a, b);
+	}
+
+	@Override
+	public float result() {
+		// TODO Auto-generated method stub
+		return (float)Math.sqrt((Math.pow(a, 2) + Math.pow(b, 2)));
+	}
+}
