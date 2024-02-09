8c1be2f260746d8014cb5258f62f874c82bfe270
gkNoel <noelgk@hotmail.com>
2024-02-09 14:18:33 -0500

    Added a difference class for subtraction

---
+++ Calculator/src/math/Difference.java
@@ -0,0 +1,15 @@
+package math;
+
+public class Difference extends Operator{
+
+	public Difference(float a, float b) {
+		super(a, b);
+	}
+
+	@Override
+	public float result() {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+}
