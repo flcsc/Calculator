86a333ae6ea89a3d2d55a0e62375fbdf9af211e2
NateTheMate14 <148722682+NateTheMate14@users.noreply.github.com>
2024-02-09 11:05:55 -0500

    Added test cases for classes in Main class.

--- Calculator/src/Main/Main.java
+++ Calculator/src/Main/Main.java
@@ -1,11 +1,22 @@
 package Main;
 
+import math.Exponent;
+import math.NthRoot;
 import math.Sum;
 
 public class Main {
 	public static void main(String[] args) {
-		Sum x = new Sum(13, 5);
-		
-		System.out.println(x.result());
+		// Testing Sum class
+		Sum sum = new Sum(13, 5);
+		System.out.print("Sum of 13 and 5: ");
+		System.out.println(sum.result());
+		// Testing Exponent class
+		Exponent exp = new Exponent(2, 3);
+		System.out.print(exp.getBase() + " to exponent " + exp.getExponent() + ": ");
+		System.out.println(exp.result());
+		// Testing NthRoot class
+		NthRoot nRoot = new NthRoot(16, 4);
+		System.out.print("Root " + nRoot.getNthRoot() + " of " + nRoot.getBase() + ": ");
+		System.out.println(nRoot.result());
 	}
 }
