c406bdf83f84f6b0f74ac34b24cd4e90ba9d5bc0
Noah Perez <noahm.p.in2017@gmail.com>
2024-02-14 14:54:12 -0500

    Added Window class

--- Calculator/src/Main/Main.java
+++ Calculator/src/Main/Main.java
@@ -1,11 +1,13 @@
 package Main;
 
+import gui.Window;
 import math.Sum;
 
 public class Main {
 	public static void main(String[] args) {
+		Window window = new Window();
 		Sum x = new Sum(13, 5);
-		
 		System.out.println(x.result());
+		while (window.isActive()) {}
 	}
 }
