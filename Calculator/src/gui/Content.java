c1df6872e8ac51e5dee638a6569b6bf5560035b5
NateTheMate14 <148722682+NateTheMate14@users.noreply.github.com>
2024-02-18 16:25:37 -0500

    Added buttons for subtraction, multiplication, division and nth-root.

--- Calculator/src/gui/Content.java
+++ Calculator/src/gui/Content.java
@@ -22,6 +22,10 @@ public class Content extends JPanel{
 		setLayout(null);
 
 		add(makeButton("+", 20, 20, 50, 50, new Color(220, 100, 100)));
+		add(makeButton("-", 20, 90, 50, 50, new Color(220, 100, 100)));
+		add(makeButton("x", 90, 20, 50, 50, new Color(220, 100, 100)));
+		add(makeButton("÷", 90, 90, 50, 50, new Color(220, 100, 100)));
+		add(makeButton("ⁿ√", 20, 160, 100, 100, new Color(100, 100, 220)));
 	}
 	
 	private JButton makeButton(String text, int x, int y, int width, int height, Color color) {
