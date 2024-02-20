65e7216738015c5999fa708d3fb6d6e6e00de9ec
gkNoel <noelgk@hotmail.com>
2024-02-20 17:27:34 -0500

    Added buttons for sin, asin, cos, acos, tan, atan, pow and percentage.

--- Calculator/src/gui/Content.java
+++ Calculator/src/gui/Content.java
@@ -26,6 +26,14 @@ public class Content extends JPanel{
 		add(makeButton("x", 90, 20, 50, 50, new Color(220, 100, 100)));
 		add(makeButton("÷", 90, 90, 50, 50, new Color(220, 100, 100)));
 		add(makeButton("ⁿ√", 20, 160, 100, 100, new Color(100, 100, 220)));
+		add(makeButton("%",90, 160 ,50 ,50 , new Color(200 ,120 ,120)));
+		add(makeButton("x\u207F", 230, 230, 50, 50, new Color(220 ,100 ,100)));
+		add(makeButton("sin", 20, 230, 50, 50, new Color(0,100,0)));
+		add(makeButton("cos", 90, 230, 50, 50, new Color(0,100,0)));
+		add(makeButton("tan", 160, 230, 50, 50, new Color(0,100,0)));
+		add(makeButton("asin", 230, 20, 50, 50, new Color(0,100,0)));
+		add(makeButton("acos", 230, 90, 50, 50, new Color(0,100,0)));
+		add(makeButton("atan", 230, 160, 50, 50, new Color(0,100,0)));
 	}
 	
 	private JButton makeButton(String text, int x, int y, int width, int height, Color color) {
